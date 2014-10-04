71757d38bdac2827342ce458d7e89010dbc5a108
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2014-10-04 13:24:34 +0200

    Added languages statistics

--- public/script.js
+++ public/script.js
@@ -5,11 +5,34 @@
 	var statistics = {};
 	var table = document.getElementById('statistics');
 
+	/**
+	 * Rewrites the statistics table in HTML
+	 */
 	function writeStatistics(){
 		table.innerHTML = "";
 		for(tag in statistics){
 			table.innerHTML += statistics[tag];
 		}
+	}
+
+	/**
+	 * Calculate percentage for each language from a tweet
+	 * @param  {Objecy} tagLanguages Languages from the tweet and their count
+	 * @return {String}              DOM with the languages statistics
+	 */
+	function calculateLangStats(tagLanguages){
+		var total = 0;
+		var dom = '<ul>';
+		for(lang in tagLanguages){
+			total += tagLanguages[lang];
+			console.log('lang: ', lang);
+			console.log('total: ', total);
+		}
+		for(lang in tagLanguages){
+			dom += "<li>" + lang + ": " + Math.round(tagLanguages[lang] / total * 100) + "% </li>";
+		}
+		dom += "</ul>";
+		return dom;
 	}
 
 	socket.emit('auth', userId);
@@ -20,7 +43,8 @@
 			for (var j = 0; j < FollowedTags.length; j++) {
 				tweetObject.updatedTags[i] = tweetObject.updatedTags[i].toLowerCase();
 				if(FollowedTags[j] === tweetObject.updatedTags[i]){
-					statistics[tweetObject.updatedTags[i]] = '<tr><td>' + tweetObject.updatedTags[i] + '</td><td>'+ tweetObject.tagsStats[tweetObject.updatedTags[i]].frequency + ' tweets/min </td></tr>';
+					var languages = calculateLangStats(tweetObject.tagsStats[tweetObject.updatedTags[i]].lang);
+					statistics[tweetObject.updatedTags[i]] = '<tr><td>' + tweetObject.updatedTags[i] + '</td><td>'+ tweetObject.tagsStats[tweetObject.updatedTags[i]].frequency + ' tweets/min </td><td>' + languages + '</td></tr>';
 				}
 			};
 		};
