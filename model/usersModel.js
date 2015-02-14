d5310ddf25e3bd939a94b5ca8251310a043722f5
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2015-02-14 16:29:53 +0000

    Store name and profile image of user in MongoDB

--- model/usersModel.js
+++ model/usersModel.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
 	user: Number,
 	token: String,
-	tokenSecret: String
+	tokenSecret: String,
+	name: String,
+	profileImage: String
 	// socket: Object, --REMOVED--
 	// pause: Boolean  --REMOVED--
 });
@@ -11,20 +13,20 @@ var userSchema = mongoose.Schema({
 var User = mongoose.model('User', userSchema);
 
 /**
- * Create a new user. If already existing, updating his socket. 
+ * Create a new user. If already existing, updating its informations
  * @param  {Number}  userId The users ID to create
  * @param  {Object}  socket The users socket       --REMOVED--
  * @param  {Boolean} pause  Users pause status     --REMOVED--
  * @return {Object}         The user once created
  */
-exports.pushUser = function(userId, token, tokenSecret){
+exports.pushUser = function(userId, token, tokenSecret, name, profileImage){
 	User.findOne({user: userId}).exec(function(err, user) {
 		if (err) {
 			return ['error', {status: 500}];
 		} 
 		else {
 			if(user == null){
-				var user = new User({user: userId, token:token, tokenSecret: tokenSecret});
+				var user = new User({user: userId, token:token, tokenSecret: tokenSecret, name: name, profileImage: profileImage});
 				user.save(function(err) {
 					if (err) return [500, err];
 					return user;
@@ -32,10 +34,9 @@ exports.pushUser = function(userId, token, tokenSecret){
 				console.log('Saved: ', user);
 			}
 			else{
-				User.update({user: userId}, {token:token, tokenSecret: tokenSecret}, {}, function(err, updatedUser) {
+				User.update({user: userId}, {token:token, tokenSecret: tokenSecret, name: name, profileImage: profileImage}, {}, function(err, numberAffected, rawResponse) {
 				    if (err) return [500, err];
-				    console.log(' updatedUser : ', updatedUser);
-				    return updatedUser;
+				    return rawResponse;
 				});
 				
 			}
