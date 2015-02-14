8599a86d4c2b75a454e4035e3c30e2f5a871f121
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2015-02-14 14:30:30 +0000

    Now store User ID in MongoDB but not socket and pause status

--- model/usersModel.js
+++ model/usersModel.js
@@ -1,26 +1,28 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-	user: Number,
-	socket: Object
+	user: Number
+	// socket: Object, --REMOVED--
+	// pause: Boolean  --REMOVED--
 });
 
 var User = mongoose.model('User', userSchema);
 
 /**
  * Create a new user. If already existing, updating his socket. 
- * @param  {Number} userId The users ID to create
- * @param  {Object} socket The users socket
- * @return {Object}        The user once created
+ * @param  {Number}  userId The users ID to create
+ * @param  {Object}  socket The users socket       --REMOVED--
+ * @param  {Boolean} pause  Users pause status     --REMOVED--
+ * @return {Object}         The user once created
  */
-exports.pushUser = function(userId, socket){
+exports.pushUser = function(userId){
 	User.findOne({user: userId}).exec(function(err, user) {
 		if (err) {
 			return ['error', {status: 500}];
 		} 
 		else {
 			if(user == null){
-				var user = new User({user: userId, socket: socket});
+				var user = new User({user: userId});
 				user.save(function(err) {
 					if (err) return [500, err];
 					return user;
@@ -28,7 +30,7 @@ exports.pushUser = function(userId, socket){
 				console.log('Saved: ', user);
 			}
 			else{
-				User.update({user: userId}, {socket: socket}, {}, function(err, updatedUser) {
+				User.update({user: userId}, {}, {}, function(err, updatedUser) {
 				    if (err) return [500, err];
 				    return updatedUser;
 				});
@@ -52,4 +54,19 @@ exports.getUserSocket = function (userId, cb){
 			cb(user.socket);
 		}
 	});
-}
+};
+
+/**
+ * Get all users
+ * @param  {Function} cb     Callback returning the socket
+ */
+exports.getAllUsers = function (cb){
+	User.find(function(err, allUsers) {
+		if (err) {
+			return ['error', {status: 500}];
+		} 
+		else {
+			cb(allUsers);
+		}
+	});
+};
