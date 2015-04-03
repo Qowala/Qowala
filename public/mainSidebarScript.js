49abaed91c438a95eb881725d1f752660923bfe8
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2015-04-03 22:23:35 +0100

    First tests to drag tags

--- public/mainSidebarScript.js
+++ public/mainSidebarScript.js
@@ -1,9 +1,11 @@
 /**
  * Displays the main controls
  */
-function MainSidebar(buttonOpenMessageEdition, inputTag){
+function MainSidebar(buttonOpenMessageEdition, inputTag, tagContainer){
 	this.buttonOpenMessageEdition = buttonOpenMessageEdition;
 	this.inputTag = inputTag;
+	this.tagContainer = tagContainer;
+	this.draggableTags = [];
 
 	this.hashtagsToTrack = [];
 }
@@ -22,6 +24,72 @@ MainSidebar.prototype.init = function(){
 		if (e.keyCode == 13) {
 			trackTag(scope);
 	    }
+	});
+
+	var dragSrcEl = null;
+
+	function handleDragStart(e) {
+		// Target (this) element is the source node.
+		this.style.opacity = '0.4';
+
+		dragSrcEl = this;
+
+		e.dataTransfer.effectAllowed = 'move';
+		e.dataTransfer.setData('text/html', this.innerHTML);
+	}
+
+	function handleDragOver(e) {
+		if (e.preventDefault) {
+		e.preventDefault(); // Necessary. Allows us to drop.
+		}
+
+		e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+		return false;
+	}
+
+	function handleDragEnter(e) {
+		// this / e.target is the current hover target.
+		this.classList.add('over');
+	}
+
+	function handleDragLeave(e) {
+		this.classList.remove('over');  // this / e.target is previous target element.
+	}
+	
+	function handleDrop(e) {
+		// this/e.target is current target element.
+
+		if (e.stopPropagation) {
+			e.stopPropagation(); // Stops some browsers from redirecting.
+		}
+
+		// Don't do anything if dropping the same column we're dragging.
+		if (dragSrcEl != this) {
+			// Set the source column's HTML to the HTML of the column we dropped on.
+			console.log('Dropping with html: ',this.innerHTML )
+			dragSrcEl.innerHTML = this.innerHTML;
+			this.innerHTML = e.dataTransfer.getData('text/html');
+		}
+
+		return false;
+	}
+
+	function handleDragEnd(e) {
+		// this/e.target is the source node.
+
+		[].forEach.call(this.draggableTags, function (draggableTag) {
+			draggableTag.classList.remove('over');
+		});
+	}
+
+	this.draggableTags = document.querySelectorAll('.draggableTag');
+	console.log('this.draggableTags : ', this.draggableTags);
+	[].forEach.call(this.draggableTags, function(draggableTag) {
+		draggableTag.addEventListener('dragstart', handleDragStart, false);
+		draggableTag.addEventListener('dragenter', handleDragEnter, false);
+		draggableTag.addEventListener('drop', handleDrop, false);
+		draggableTag.addEventListener('dragend', handleDragEnd, false);
 	});
 }
 
