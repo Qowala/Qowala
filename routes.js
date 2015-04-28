95447f87d55420a77ed49d207a3669919f4d5189
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2015-04-28 22:39:52 +0200

    Added logout button

--- routes.js
+++ routes.js
@@ -22,7 +22,13 @@ function routes (app){
 		passport.authenticate('twitter', { 
 			successRedirect: '/dashboard',
  			failureRedirect: '/' 
-		}));
+		})
+	);
+
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	});
 
 	// Simple route middleware to ensure user is authenticated.
 	// Use this route middleware on any resource that needs to be protected. If
