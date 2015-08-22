8ac648ebcd12a9a06749d37f01dce551aef83e82
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2015-08-22 20:13:00 +0200

    Add deprecated browser popup.
    TG-69 #ready-for-test
    By the way updates to the latest version of Font Awesome to use the
    browser icons.

--- public/js/main.js
+++ public/js/main.js
@@ -60,6 +60,14 @@ var dashboard = (function (socket){
       }
     });
 
+    // Check if browser version is recent enough
+    try {
+      var promise = new Promise(function(){
+      });
+    } catch (error) {
+      var popup = document.getElementById('deprecatedBrowserPopup');
+      popup.style.display = 'block';
+    }
     callback();
   }
 
