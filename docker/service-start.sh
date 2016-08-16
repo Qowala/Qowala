7274e0a195ff19b65bb480b442e7450d406e13c1
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-08-16 17:23:53 +0200

    Change nodejs start to forever start in docker

--- docker/service-start.sh
+++ docker/service-start.sh
@@ -4,5 +4,4 @@
 cd /var/app
 
 # Start NodeJS server
-#forever -l forever.log -ao stdout.log -e stderr.log start server.js
-node server.js
+forever -l forever.log -ao stdout.log -e stderr.log server.js
